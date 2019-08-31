342e151424cc78457a148662ae0c5023cf30e129
seungCheolCha <cscculture95@gmail.com>
2019-09-01 01:50:14 +0900

    템플릿을 이용한 트리 구현 프로젝트(ver 2.0) 최종 수정일 : 190901 수정자 : 차 승철
    수정 내용
    cTreeNode에 부모 노드 정보도 저장할 수 있게 m_pParent 멤버변수를 추가
    Insert 수정(부모 노드도 저장이 가능하게)
    트리 내용을 출력을 통해 확인할 수 있게 만드는 OutputTree (기본값은 루트노드부터 아래로 차례차례) 구현
    소멸자 호출 시 Clear 메소드를 통해서 메모리 릭이 안 나게 수정

--- CPP_Template_Tree/CTree.h
+++ CPP_Template_Tree/CTree.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
 enum NODE_INSERT_TYPE
 {
 	NIT_SIBLING,
@@ -18,6 +23,7 @@ private:
 	{
 		m_pSibling = nullptr;
 		m_pChild = nullptr;
+		m_pParent = nullptr;
 	}
 	~CTreeNode()	{}
 
@@ -25,6 +31,7 @@ private:
 private : 
 	CTreeNode<Key, Value>* m_pSibling;			// 형제 노드
 	CTreeNode<Key, Value>* m_pChild;				// 자식노드
+	CTreeNode<Key, Value>* m_pParent;
 	Key m_Key;
 	Value m_Value;
 };
@@ -34,15 +41,15 @@ private :
 template <typename Key, typename Value>
 class CTree
 {
-private : 
+private:
 	typedef CTreeNode<Key, Value> NODE;
 	typedef CTreeNode<Key, Value>* PNODE;
 
-private : 
+private:
 	PNODE m_pRoot;			// 루트 노드(제일 맨 위에 있는 노드)
 	int m_nSize;					// 트리에 쌓인 노드
 
-public : 
+public:
 	CTree()
 	{
 		m_pRoot = nullptr;
@@ -51,12 +58,14 @@ public :
 
 	~CTree()
 	{
-
+		Clear();
 	}
 
-public : 
+public:
+	// 노드를 생성해서 이를 넣는 메소드이다.
+	// 이 때 노드의 넣는 타입 기본값은 형제값으로 넣게 되어있다.
 	void Insert(const Key& _key, const Value& _val,
-			const Key& BaseKey = Key(), NODE_INSERT_TYPE eType = NIT_SIBLING)
+		const Key& BaseKey = Key(), NODE_INSERT_TYPE eType = NIT_SIBLING)
 	{
 		// 새로운 노드 생성
 		PNODE newNode = new NODE;
@@ -69,34 +78,53 @@ public :
 		{
 			// 중요! 루트 노드는 오로지 자식 노드만 가진다.
 			m_pRoot = newNode;
-			++m_nSize;
 		}
+
+		// 루트 노드가 있는 경우
 		else
 		{
 			// 붙여줄 기준 노드를 찾아준다.
 			PNODE pBaseNode = FindNode(BaseKey, m_pRoot);
 
+			// 만약 노드가 아무 것도 없다면
 			if (!pBaseNode)
 			{
 				delete newNode;
 				return;
 			}
+
+			// 만약 베이스노드가 루트 노드에 
+			// 새로운 노드를 넣는 방식이 자식이 아닌 형제라면
+			// 말이 될 수 없는 상황(루트 노드는 반드시 하나만 있어야하지!)이기 
+			// 때문에 아무 작업도 하지 않는다.
 			else if (pBaseNode == m_pRoot && eType == NIT_SIBLING)
 			{
 				delete newNode;
 				return;
 			}
-			// 형제노드? or 자식노드로 추가?
+
+			// 형제노드(베이스 노드가 루트 노드는 아님)
+			// or 자식노드로 추가
 			switch (eType)
 			{
 			case NIT_SIBLING:
 				// 형제로 추가할 때 만약 형제가 없다면 바로 형제로 추가
 				if (!pBaseNode->m_pSibling)
+				{
+					// 지금 가리키고 있는 노드의 형제 노드에 새로운 노드를 넣는다.
 					pBaseNode->m_pSibling = newNode;
+
+					// 지금 가리키고 있는 노드와 새로운 노드는 형제기 때문에,
+					// 부모 노드가 같다.
+					newNode->m_pParent = pBaseNode->m_pParent;
+				}
 				else
 				{
 					// 기존 형제를 받아온다.
 					PNODE pSibling = pBaseNode->m_pSibling;
+
+					// 형제와 같은 부모 노드를 가진다.
+					newNode->m_pParent = pSibling->m_pParent;
 
 					// 새로 생성한 노드의 형제로 기존의 형제노드를 준다.
 					newNode->m_pSibling = pSibling;
@@ -108,11 +136,21 @@ public :
 			case NIT_CHILD:
 				// 자식으로 추가할 때 만약 자식이 없다면 바로 자식으로 추가
 				if (!pBaseNode->m_pChild)
+				{
+					// 현재 가리키고 있는 노드의 자식 노드로 추가
 					pBaseNode->m_pChild = newNode;
+
+					// 새로운 노드의 부모노드는 현재 가리키고 있는 노드이다.
+					newNode->m_pParent = pBaseNode;
+				}
+					
 				else
 				{
 					// 기존 자식을 받아온다.
 					PNODE pChild = pBaseNode->m_pChild;
+
+					// 새로 추가된 노드는 기존 자식 노드의 부모 노드와 같다.
+					newNode->m_pParent = pChild->m_pParent;
 
 					// 새로 생성한 노드의 자식으로 기존의 자식 노드를 준다.
 					newNode->m_pSibling = pChild;
@@ -123,7 +161,16 @@ public :
 				break;
 			}
 		}
+		m_nSize++;
+	}
 
+	// 루트 노드 하 모든 노드를 출력해주는 메소드
+	void OutputTree()
+	{
+		if (IsEmpty())
+			return;
+
+		OutputTree(m_pRoot);
 	}
 
 	bool IsEmpty() const
@@ -136,24 +183,77 @@ public :
 		return m_nSize;
 	}
 
+	void Clear()
+	{
+		Clear(m_pRoot);
 
-private : 
+		m_pRoot = nullptr;
+		m_nSize = 0;
+	}
+
+private:
 	// 재귀적인 방식으로 노드를 찾아준다.
 	PNODE FindNode(const Key& _key, PNODE  pNode)
 	{
 		PNODE pResult = nullptr;
 
 		if (pNode->m_Key == _key)
-			return PNODE;
+			return pNode;
 
 		// 형제 노드가 있다면 
-		if(pNode->m_pSibling)
+		if (pNode->m_pSibling)
 			pResult = FindNode(_key, pNode->m_pSibling);
 
+		// 자식 노드가 있고 리절트 노드가 nullptr이면
+		// 자식 노드에서 매개변수 키값과 동일한 키값을 가진 노드를 검색한다.
 		if (pNode->m_pChild && !pResult)
 			pResult = FindNode(_key, pNode->m_pChild);
 
 		return pResult;
 	}
-};
 
+	// 특정 노드를 받아서 그 하위 and 형제 노드를 모두 출력해주는 메소드
+	void OutputTree(PNODE pNode)
+	{
+		cout << "===============" << pNode->m_Key << "===============" << endl;
+		cout << "Key : " << pNode->m_Key << 
+			"\tValue : " << pNode->m_Value << endl;
+
+		cout << "부모 노드 : ";
+		if (!pNode->m_pParent)
+			cout << "없음" << endl;
+
+		else
+			cout << pNode->m_pParent->m_Key << endl;
+
+		cout << "형제 : ";
+		if (!pNode->m_pSibling)
+			cout << "없음" << endl;
+		else
+			cout << pNode->m_pSibling->m_Key << endl;
+
+		cout << "자식 : ";
+		if (!pNode->m_pChild)
+			cout << "없음" << endl;
+		else
+			cout << pNode->m_pChild->m_Key << endl;
+
+		if (pNode->m_pSibling)
+			OutputTree(pNode->m_pSibling);
+
+		cout << endl;
+
+		if (pNode->m_pChild)
+			OutputTree(pNode->m_pChild);
+	}
+
+	void Clear(PNODE pNode)
+	{
+		if (pNode->m_pSibling)
+			Clear(pNode->m_pSibling);
+		if(pNode->m_pChild)
+			Clear(pNode->m_pChild);
+
+		delete pNode;
+	}
+};
--- CPP_Template_Tree/main.cpp
+++ CPP_Template_Tree/main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include <string>
 
 #include "CTree.h"
 using namespace std;
 
+/*
+템플릿을 이용한 트리 구현 프로젝트(ver 2.0)
+최종 수정일 : 190901
+수정자 : 차 승철
+
+2.0 수정 내용
+cTreeNode에 부모 노드 정보도 저장할 수 있게 m_pParent 멤버변수를 추가
+
+Insert 수정(부모 노드도 저장이 가능하게)
+
+트리 내용을 출력을 통해 확인할 수 있게 만드는 OutputTree (기본값은 
+루트노드부터 아래로 차례차례) 구현
+
+소멸자 호출 시 Clear 메소드를 통해서 메모리 릭이 안 나게 수정
+*/
+
 int main()
 {
+	CTree<string, int> cTree;
 
+
+	// 루트 노드 
+	cTree.Insert("김두한", 10);
+
+	// 서브 노드
+	cTree.Insert("시라소니", 30, "김두한", NIT_CHILD);
+	cTree.Insert("이정재", 50, "시라소니", NIT_SIBLING);
+	cTree.Insert("삼수", 100, "이정재", NIT_SIBLING);
+	cTree.Insert("정진영", 35, "삼수", NIT_CHILD);
+	cTree.Insert("심영이 어머니", 28, "시라소니", NIT_CHILD);
+	cTree.Insert("조병옥", 28, "심영이 어머니", NIT_SIBLING);
+
+	cTree.OutputTree();
 
 	return 0;
 }
